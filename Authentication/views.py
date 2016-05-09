d20f4e9521aaf12c726010100661c65783b81948
Rohit Sarathy <rohit@sarathy.org>
2016-05-09 07:00:14 -0500

    logins

--- Authentication/views.py
+++ Authentication/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.shortcuts import render, render_to_response
+from Authentication.forms import LoginForm
 
-# Create your views here.
 def login(request):
-    pass
+    userid = ""
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            userid = form.cleaned_data["username"]
+            passwd = form.cleaned_data["password"]
+            user = authenticate(username=userid, password=passwd)
+            if user is not None and user.is_active:
+                return render(request, "index.html", {"username": userid})
+            else:
+                print "Invalid login."
+    else:
+        form = LoginForm()
+    return render(request, "login.html", {"form": form, "username": userid})
--- ClerkLog/forms.py
+++ ClerkLog/forms.py
@@ -4,8 +4,8 @@ class ClerkLog(forms.Form):
     clerk_name = forms.CharField(max_length=64, required=True)
     entry_subject = forms.CharField(max_length=256, required=True)
     phone_number = forms.NumberInput()
-    description = forms.TextInput()
-
+    description = forms.Textarea()
+    # date
     prostaff = forms.CharField(max_length=64)
     radvisor = forms.CharField(max_length=64)
 
